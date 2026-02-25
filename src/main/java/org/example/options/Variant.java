359115f857aa1c1e162ebe903aff0b5c818f2360
kardach <oskarkardach@protonmail.com>
2026-02-25 20:18:38 +0100

    code reformating

--- src/main/java/org/example/options/Variant.java
+++ src/main/java/org/example/options/Variant.java
@@ -1,7 +1,7 @@
 package org.example.options;
 
-public record Variant(String name, int boardSize, int piecesPerSide, boolean lightSquareOnNearRight, PiecesPlacement piecesPlacement,
-                      FirstMove firstMove) {
+public record Variant(String name, int boardSize, int piecesPerSide, boolean lightSquareOnNearRight,
+                      PiecesPlacement piecesPlacement, FirstMove firstMove) {
     public enum PiecesPlacement {
         ON_WHITE,
         ON_BLACK,
