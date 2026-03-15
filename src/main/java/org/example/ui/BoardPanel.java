894a69972f498826d47accb2d659d084f9a28299
kardach <oskarkardach@protonmail.com>
2026-03-15 12:23:07 +0100

    highlight selected square

--- src/main/java/org/example/ui/BoardPanel.java
+++ src/main/java/org/example/ui/BoardPanel.java
@@ -1,6 +1,7 @@
 package org.example.ui;
 
 import org.example.model.Board;
+import org.example.model.Move;
 import org.example.model.Piece;
 import org.example.model.Square;
 
@@ -11,7 +12,9 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
 class BoardPanel extends JPanel {
-    public BoardPanel(Board board, int size) {
+    private JButton selectedSquare;
+
+    public BoardPanel(Board board, Move move, int size) {
         int boardSize = board.getSize();
 
         setBounds(200, 0, size, size);
@@ -27,10 +30,14 @@ class BoardPanel extends JPanel {
                 jButton.setMargin(new Insets(0, 0, 0, 0));
                 jButton.setRolloverEnabled(false);
                 jButton.addMouseListener(new MouseListener() {
+                    private JButton jButton;
+                    private Move move;
                     private int row;
                     private int col;
 
-                    private MouseListener init(int row, int col) {
+                    private MouseListener init(JButton jButton, Move move, int row, int col) {
+                        this.jButton = jButton;
+                        this.move = move;
                         this.row = row;
                         this.col = col;
                         return this;
@@ -48,7 +55,23 @@ class BoardPanel extends JPanel {
 
                     @Override
                     public void mouseReleased(MouseEvent e) {
+
                         System.out.println("row = " + row +", col = " + col);
+                        if(move.isEmpty()) {
+                            move.begin(row, col);
+                        } else {
+                            move.add(row, col);
+                        }
+
+                        if(selectedSquare != null) {
+                            SquareButtonUI ui = (SquareButtonUI) selectedSquare.getUI();
+                            ui.setSelected(false);
+                            selectedSquare.revalidate();
+                            selectedSquare.repaint();
+                        }
+                        selectedSquare = jButton;
+                        SquareButtonUI ui = (SquareButtonUI) selectedSquare.getUI();
+                        ui.setSelected(true);
                     }
 
                     @Override
@@ -60,7 +83,7 @@ class BoardPanel extends JPanel {
                     public void mouseExited(MouseEvent e) {
 
                     }
-                }.init(row, col));
+                }.init(jButton, move, row, col));
                 add(jButton);
             }
         }
@@ -68,6 +91,7 @@ class BoardPanel extends JPanel {
 
     static class SquareButtonUI extends BasicButtonUI {
         private Square square;
+        private boolean selected;
 
         public void setSquare(Square square) {
             this.square = square;
@@ -77,10 +101,16 @@ class BoardPanel extends JPanel {
             return square;
         }
 
+        public void setSelected(boolean selected) {
+            this.selected = selected;
+        }
+
         private void paint(Graphics g, Dimension size) {
             Color squareColor = getSquare().getColor() == Square.Color.BLACK ? new Color(139, 69, 19)
                     : new Color(255, 228, 196);
-
+            if(selected) {
+                squareColor = Color.GREEN;
+            }
             g.setColor(squareColor);
             g.fillRect(0, 0, size.width, size.height);
 
@@ -91,6 +121,13 @@ class BoardPanel extends JPanel {
                 g.setColor(pieceColor);
                 g.fillOval(0, 0, size.width, size.height);
             }
+
+//            if(selected) {
+//                Graphics2D g2 = (Graphics2D) g;
+//                g2.rotate(Math.toRadians(45));
+//                g2.setColor(new Color(255, 0, 0, 128));
+//                g2.fillRect(0, 0, 100, 10);
+//            }
         }
 
         @Override
