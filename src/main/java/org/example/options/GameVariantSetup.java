f4f4568ae576d4ae0f23ba75dc31956067c3617e
kardach <oskarkardach@protonmail.com>
2026-03-23 03:28:35 +0100

    refactoring

--- src/main/java/org/example/options/GameVariantSetup.java
+++ src/main/java/org/example/options/GameVariantSetup.java
@@ -1,18 +1,24 @@
 package org.example.options;
 
 import java.util.Map;
+import java.util.SortedMap;
+import java.util.TreeMap;
 
 import static java.util.Map.entry;
 
 public class GameVariantSetup {
-    private final Map<String, Variant> variants = Map.ofEntries(
-            entry("International/Polish", new Variant("International/Polish", 10,
-                    20, true, Variant.PiecesPlacement.ON_BLACK, Variant.FirstMove.WHITE,
-                    true, true, Variant.Capture.MAXIMUM)),
-            entry("Ghanaian/damii", new Variant("Ghanaian/damii", 10, 20,
-                    false, Variant.PiecesPlacement.ON_WHITE, Variant.FirstMove.BLACK, true,
-                    true, Variant.Capture.ALL))
-    );
+    private final SortedMap<String, Variant> variants = new TreeMap<>();
+
+    public GameVariantSetup() {
+        variants.putAll(Map.ofEntries(
+                entry("International/Polish", new Variant("International/Polish", 10,
+                        20, true, Variant.PiecesPlacement.ON_BLACK,
+                        Variant.FirstMove.WHITE, true, true, Variant.Capture.MAXIMUM)),
+                entry("Ghanaian/damii", new Variant("Ghanaian/damii", 10, 20,
+                        false, Variant.PiecesPlacement.ON_WHITE, Variant.FirstMove.BLACK,
+                        true, true, Variant.Capture.ALL))
+        ));
+    }
 
     private String selected;
 
