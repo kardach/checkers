9d1be1b2e06e768ab6239daf6551ebd9b71a6400
kardach <oskarkardach@protonmail.com>
2026-04-06 14:52:45 +0200

    add test suite

---
+++ src/test/java/TestSuite.java
@@ -0,0 +1,6 @@
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectPackages("back_capture.flying_kings.max_capture")
+public class TestSuite {}
