91b3a12645bd45d4d2df971ee5c8aa09f200b503
kardach <oskarkardach@protonmail.com>
2026-07-25 14:37:29 +0200

    unit test square

--- src/test/java/checkers/model/SquareTest.java
+++ src/test/java/checkers/model/SquareTest.java
@@ -1,53 +1,84 @@
 package checkers.model;
 
-import checkers.model.Board;
-import checkers.model.Color;
-import checkers.model.Piece;
-import checkers.model.Square;
-import checkers.support.ReplaceCamelCase;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayNameGeneration;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
+import checkers.support.CustomNameGenerator;
+import org.jspecify.annotations.NullMarked;
+import org.junit.jupiter.api.*;
+
+import java.util.List;
+import java.util.NoSuchElementException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@DisplayNameGeneration(ReplaceCamelCase.class)
+@NullMarked
+@DisplayNameGeneration(CustomNameGenerator.class)
 class SquareTest {
 
-    private final Square square = new Square(Color.BLACK, null);
-    private final Piece piece = new Piece(Color.WHITE);
+    final Position topLeft = new Position(0, 0);
+    final Position topRight = new Position(0, 1);
+    final Position bottomLeft = new Position(1, 0);
+    final Position bottomRight = new Position(1, 1);
+    final List<PiecePlacement> piecePlacements = List.of(
+            new PiecePlacement(Color.BLACK, Type.MAN, topLeft),
+            new PiecePlacement(Color.BLACK, Type.MAN, bottomRight)
+    );
+    final Board board = new Board(2, Color.BLACK, piecePlacements, false, false);
 
-    @Nested
-    class SquareWithoutPieceTest {
-
-        @Test
-        void removePiece() {
-            assertNull(square.removePiece());
-        }
-
-        @Test
-        void hasAPiece() {
-            assertFalse(square.hasPiece());
-        }
+    @Test
+    void hasPieceShouldReturnCorrectly() {
+        assertTrue(board.at(topLeft).hasPiece());
+        assertTrue(board.at(bottomRight).hasPiece());
+        assertFalse(board.at(topRight).hasPiece());
+        assertFalse(board.at(bottomLeft).hasPiece());
     }
 
-    @Nested
-    class SquareWithPieceTest {
+    @Test
+    void getPieceShouldThrowWhenPieceIsNotPresent() {
+        assertThrows(NoSuchElementException.class, () -> board.at(topRight).getPiece());
+    }
 
-        @BeforeEach
-        void init() {
-            square.placePiece(piece);
-        }
+    @Test
+    void getPieceShouldNotThrowWhenPieceIsPresent() {
+        assertDoesNotThrow(() -> board.at(topLeft).getPiece());
+    }
 
-        @Test
-        void removePiece() {
-            assertEquals(piece, square.removePiece());
-        }
+    @Test
+    void getPieceShouldReturnPieceAndNotRemoveIt() {
+        board.at(topLeft).getPiece();
+        assertTrue(board.at(topLeft).hasPiece());
+    }
 
-        @Test
-        void hasAPiece() {
-            assertTrue(square.hasPiece());
-        }
+    @Test
+    void removePieceShouldThrowWhenPieceIsNotPresent() {
+        assertThrows(NoSuchElementException.class, () -> board.at(topRight).removePiece());
+    }
+
+    @Test
+    void removePieceShouldNotThrowWhenPieceIsPresent() {
+        assertDoesNotThrow(() -> board.at(topLeft).removePiece());
+    }
+
+    @Test
+    void removePieceShouldRemovePiece() {
+        board.at(topLeft).removePiece();
+        assertFalse(board.at(topLeft).hasPiece());
+    }
+
+    @Test
+    void placePieceShouldThrowWhenPieceIsPresent() {
+        Board.Piece piece = board.at(topLeft).removePiece();
+        assertThrows(IllegalStateException.class, () -> board.at(bottomRight).placePiece(piece));
+    }
+
+    @Test
+    void placePieceShouldNotThrowWhenPieceIsNotPresent() {
+        Board.Piece piece = board.at(topLeft).removePiece();
+        assertDoesNotThrow(() -> board.at(topRight).placePiece(piece));
+    }
+
+    @Test
+    void placePieceShouldPlacePiece() {
+        Board.Piece piece = board.at(topLeft).removePiece();
+        board.at(topRight).placePiece(piece);
+        assertTrue(board.at(topRight).hasPiece());
     }
 }
