3787b241e1749507634b3bdfa9840f50741e4b60
kardach <oskarkardach@protonmail.com>
2026-07-25 16:46:27 +0200

    fix naming

--- src/test/java/checkers/model/PositionTest.java
+++ src/test/java/checkers/model/PositionTest.java
@@ -1,15 +1,20 @@
 package checkers.model;
 
+import checkers.support.CustomNameGenerator;
+import org.jspecify.annotations.NullMarked;
+import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@NullMarked
+@DisplayNameGeneration(CustomNameGenerator.class)
 public class PositionTest {
 
     private final Position position = new Position(0, 0);
 
     @Test
-    void direction() {
+    void getDirectionsShouldReturnCorrectly() {
         assertEquals(Direction.TOP, position.getDirection(new Position(-1, 0)));
         assertEquals(Direction.TOP_RIGHT, position.getDirection(new Position(-1, 1)));
         assertEquals(Direction.RIGHT, position.getDirection(new Position(0, 1)));
@@ -22,7 +27,7 @@ public class PositionTest {
     }
 
     @Test
-    void translate() {
+    void translateShouldReturnCorrectly() {
         assertEquals(new Position(-1, 0), position.translate(Direction.TOP, 1));
         assertEquals(new Position(-1, 1), position.translate(Direction.TOP_RIGHT, 1));
         assertEquals(new Position(0, 1), position.translate(Direction.RIGHT, 1));
